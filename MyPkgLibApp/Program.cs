a725c90590146b2f252908d1170759abd630238d
Alex Vincent <alex@far2cool.com>
2019-06-14 09:34:30 +0200

    simple test add added for testing snupkg/sourcelink

---
+++ MyPkgLibApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CasCap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new TestClass1();
+            obj.ThrowDivideByZeroException();
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
